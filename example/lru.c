9bf2ea2b9b1cf12d3bc093f983cbe01d465319b3
13521982586@163.com <>
2020-08-07 09:05:22 +0800

    About LRU

--- example/lru.c
+++ example/lru.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 typedef struct c_lru_list {
 	int			page;
 	int			length;
 	struct c_lru_list	*left;
 	struct c_lru_list	*right;
-	struct c_lru_list	*pos;
 } c_lru_list;
 
 c_lru_list *c_lru_list_create( void )
 {
 	c_lru_list *list = (c_lru_list *) malloc( sizeof(c_lru_list) );
 	list->length	= 0;
-	list->left	= list->right = list->pos = NULL;
+	list->left	= list->right = NULL;
 	return(list);
 }
 
 
-bool c_lru_list_exist( c_lru_list *list, int page )
+c_lru_list *c_lru_list_search( c_lru_list *list, int page )
 {
-	bool		status	= false;
+	c_lru_list	*pos	= NULL;
 	c_lru_list	*temp	= list->right;
 	printf( "当前页表项: [ " );
 	while ( temp )
@@ -30,43 +30,38 @@ bool c_lru_list_exist( c_lru_list *list, int page )
 		printf( "%d ", temp->page );
 		if ( temp->page == page )
 		{
-			list->pos	= temp;
-			status		= true;
+			pos = temp;
 		}
 		temp = temp->right;
 		if ( temp == list->right )
 			break;
 	}
 	printf( "]" );
-	return(status);
+	return(pos);
 }
 
 
-void c_lru_list_alloc( c_lru_list *list, int length, int page, int status )
+void c_lru_list_alloc( c_lru_list *list, c_lru_list *pos, int length, int page )
 {
-	if ( status )
+	if ( pos )
 	{
-		if ( list->pos == list->left )
+		if ( pos == list->left )
 		{
-			list->left = list->pos->left;
-			list->right = list->pos;
-		}
-		else if ( list->pos == list->right )
+			list->left	= pos->left;
+			list->right	= pos;
+		}else if ( pos == list->right )
 		{
-		
-		}
-		else
-		{
-			list->pos->left->right	= list->pos->right;
-			list->pos->right->left	= list->pos->left;
+		}else {
+			pos->left->right	= pos->right;
+			pos->right->left	= pos->left;
 
-			list->pos->left		= list->left;
-			list->pos->right	= list->right;
+			pos->left	= list->left;
+			pos->right	= list->right;
 
-			list->left->right	= list->pos;
-			list->right->left	= list->pos;
+			list->left->right	= pos;
+			list->right->left	= pos;
 
-			list->right = list->pos;
+			list->right = pos;
 		}
 	}else{
 		c_lru_list *node = (c_lru_list *) malloc( sizeof(c_lru_list) );
@@ -135,20 +130,20 @@ int main( void )
 	int lose = 0;
 	for ( int idx = 0; idx < num; idx++ )
 	{
-		bool status = c_lru_list_exist( list, arr[idx] );
-		c_lru_list_alloc( list, length, arr[idx], status );
-		if ( status )
+		c_lru_list *pos = c_lru_list_search( list, arr[idx] );
+		c_lru_list_alloc( list, pos, length, arr[idx] );
+		if ( pos )
 		{
 			printf( "\n页号[%d]在页表中,不缺页\n", arr[idx] );
 		}else{
 			lose++;
 			printf( "\n页号[%d]不在页表中,缺页\n", arr[idx] );
 		}
-		c_lru_list_exist( list, arr[idx] );
-		printf("\n\n");
+		c_lru_list_search( list, arr[idx] );
+		printf( "\n\n" );
 	}
 
-	printf( "经计算,共缺[%d]页,缺页率为: [%d/%d]\n\n", lose, lose, num );
+	printf( "经计算,共缺[%d]页,缺页率为: [%.2f]\n\n", lose, round(lose * 100 / num) );
 
 	return(0);
 }
